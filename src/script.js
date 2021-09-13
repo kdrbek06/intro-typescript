6bfc2f7a4765a161073028c157e17001b9592911
kdrbek06 <kdrbek@gmail.com>
2021-09-13 15:54:06 +0200

    Add tsconfig.json file and compile the .ts as .js file

--- src/script.js
+++ src/script.js
@@ -1,25 +1,20 @@
-(function() {
+(function () {
     document.getElementById('guess-form').addEventListener("submit", compareUserGuess);
-
-    function compareUserGuess (event) {
+    function compareUserGuess(event) {
         event.preventDefault();
-
-        const secretNumber = generateSecretNumber();
-        const userNumber = document.getElementById('guess').value;
-
+        var secretNumber = generateSecretNumber();
+        var userNumber = document.getElementById('guess').value;
         if (secretNumber == userNumber) {
             alert("Awesome! You number " + userNumber + " was correct. You can be named many things, hungry not being one of them.");
-        } else {
+        }
+        else {
             alert("Bummer... You guessed " + userNumber + " and the secret number was " + secretNumber + ".");
         }
     }
-
     function generateSecretNumber() {
-        const secretNumber = getRandomArbitrary(1, 22);
-
+        var secretNumber = getRandomArbitrary(1, 22);
         return secretNumber;
     }
-
     function getRandomArbitrary(min, max) {
         return Math.round(Math.random() * (max - min) + min);
     }
