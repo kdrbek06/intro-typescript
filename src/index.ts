9a5e4337fec4cf2f7133a5e747ce6e64026a506f
kdrbek06 <kdrbek@gmail.com>
2021-09-15 00:59:37 +0200

    Add webpack to project

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@
     document.getElementById('guess-form').addEventListener("submit", compareUserGuess);
 
     // function with void can't return anything
-    function compareUserGuess (event):void {
+    function compareUserGuess (event : any) :void {
         event.preventDefault();
 
         const secretNumber = generateSecretNumber();
-        const userNumber = (document.getElementById('guess') as HTMLInputElement).value;
+        const userNumber = parseInt((document.getElementById('guess') as HTMLInputElement).value);
 
-        if (secretNumber == parseInt(userNumber)) {
-            alert("Awesome! You number " + userNumber + " was correct. You can be named many things, hungry not being one of them.");
+        if (secretNumber == userNumber) {
+            alert("Awesome! You number " + userNumber + " was correct./////// You can be named many things, hungry not being one of them.");
         } else {
             alert("Bummer... You guessed " + userNumber + " and the secret number was " + secretNumber + ".");
         }
